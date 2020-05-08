92b70fdeb1b791db966eaf27bb59477a54a2926e
holdlg <holdlg@live.com>
2020-05-08 12:17:29 +0800

    test ssh key

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use structopt::StructOpt;
+use std::io;
 
 #[derive(StructOpt)]
 struct Cli {
@@ -9,9 +10,23 @@ struct Cli {
 }
 
 fn main() {
+    let abc2 = String::from("xxx");
+    let mut abc = String::from("xxx");
     println!("Hello, world!");
+    println!("Hello, world!");
+    io::stdin().read_line(&mut abc).expect("Failed to read line");
+    println!("{}", abc);
+    println!("{}", abc.trim() == abc2);
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
     println!("{}", pattern);
     println!("{}", path);
+
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line)
+        }
+    }
 }
